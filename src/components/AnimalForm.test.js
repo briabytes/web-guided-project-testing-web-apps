fc7acb4c8cec974a844242dbf47fe3e92f40cae2
Josh Jacobson <joshjacobson14@gmail.com>
2020-12-09 17:53:50 -0800

    lecture work-in-progress

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -3,4 +3,6 @@ import {render, screen} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import AnimalForm from "./AnimalForm";
 
-
+test("renders AnimalForm component without errors", () => {
+    render(<AnimalForm/>);
+});
