d18ae49fbfffc7978fba26a00a8e0cba5a9d2e43
Josh Jacobson <joshjacobson14@gmail.com>
2020-12-09 17:25:38 -0800

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,14 @@
 import React from "react";
-import {render} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import App from "./App";
 
 test("renders App without errors", () => {
     render(<App />);
+})
+
+test("renders the app header", () => {
+    render(<App />);
+
+    const get
+
 })
