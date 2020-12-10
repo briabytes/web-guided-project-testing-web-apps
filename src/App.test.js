23d130bce7d8c1e39c4db1ee461cd788de771a86
Josh Jacobson <joshjacobson14@gmail.com>
2020-12-09 17:27:16 -0800

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -9,5 +9,5 @@ test("renders App without errors", () => {
 test("renders the app header", () => {
     render(<App />);
 
-    const header = screen.getAllByText("ADD NEW ANIMAL");
+    const header = screen.getAllByText("Add New Animal");
 })
